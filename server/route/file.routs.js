df2bdf3847aee424154ab56822ccf79f2ad1f008
abhinav <abhinavzm99@gmail.com>
2025-07-02 23:15:32 +0530

    user model

--- server/route/file.routs.js
+++ server/route/file.routs.js
@@ -1,4 +1,10 @@
 import express from 'express';
 const app=express();
-route=app.Router
+
+import { Router } from 'express';
+import upload from '../middleware/upload.js';
+import {deletefile,downloadFile,generateQR,generateShareShortenLink,getDownloadCount,getFileDetails,getUserFiles, resolveShareLink, searchFiles, sendLinkEmail, showUserFiles, updateFileExpiry, updateFilePassword, updateFileStatus, uploadFiles, verifyFilePassword ,  } from "../controller/file.controller.js";
+
+
+const router=Router();
 
